971011e9cf4a8bfb5d129541b3d83e91d52e31c7
Daniel Ramos <danielramosacosta1@gmail.com>
2024-12-26 15:00:25 +0000

    align with enunciado

--- test.ts
+++ test.ts
@@ -13,8 +13,8 @@ const browser = await puppeteer.launch({
 const views = ["desktop", "tablet", "phone"];
 const pages = ["index", "tema-del-mes", "blog", "post"];
 
-const skipViews: string[] = [];
-const skipPages: string[] = [];
+const skipViews: string[] = ["tablet", "phone"];
+const skipPages: string[] = ["tema-del-mes", "blog", "post"];
 
 const result = await Promise.all(
   pages
